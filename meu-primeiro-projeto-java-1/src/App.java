222f416ec0f27838299fcb93bb13726c6810b7cb
Leo Gouvea <leonardo.j.gouvea@outlook.com>
2025-02-23 13:33:14 -0300

    Olá, estou fazendo meu commit número 1.

--- meu-primeiro-projeto-java-1/src/App.java
+++ meu-primeiro-projeto-java-1/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Esse é meu primeiro projeto, olá!");
+        System.out.println("Esse é meu primeiro projeto, olá! :)");
     }
 }
